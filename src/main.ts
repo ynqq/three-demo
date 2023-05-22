8f18b5eb7ef0eb923a95e8bf96c14dd063bddec7
NEODC\qinjiahui <qinjiahui@neotrident.com>
2023-05-22 18:09:07 +0800

    ✨ feat: ..

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import "./assets/main.css";
 import router from "./router";
-import "ant-design-vue/dist/antd.min.css";
 import "uno.css";
 import { createApp } from "vue";
 import App from "./App.vue";
